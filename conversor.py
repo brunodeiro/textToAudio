a39eec80ddddd5f45709d85e0a71cb2f83edee4d
Bruno <bruno_deiro@live.com>
2022-10-17 23:04:14 -0300

    Conversor de texto para audio

---
+++ conversor.py
@@ -0,0 +1,15 @@
+from gtts import gTTS
+from playsound import playsound
+
+# Transformando texto em áudio com Python
+
+audio = 'audio.mp3'
+idioma = 'pt-br'
+
+sp = gTTS(
+    text='Meu primeiro áudio gerado com Python!',
+    lang=idioma
+)
+
+sp.save(audio)
+playsound(audio)
